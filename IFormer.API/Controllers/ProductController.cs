a9749e2eaf164def75901705c472852d2d75215a
Navaneeth Sankaradevan <navneethkrishnaa@outlook.com>
2018-05-19 16:02:26 +0530

    Some basic changes
    Renamed folders and changed controller implementation

---
+++ IFormer.API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Online.Model;
+using System.Threading.Tasks;
+
+namespace onlinestore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        //[HttpGet]
+        public async Task< List< Product>> Get()
+        {
+            return await productRepository.GetAllAsync();
+        }
+    }
+}
